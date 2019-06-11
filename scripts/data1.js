5ca28904bad98ae844d6c987565ca08def24c0bb
Animesh Dhengre <dhengreani18@gmail.com>
2019-06-11 13:43:58 +0530

    added dor reading bidder inputs

--- scripts/data1.js
+++ scripts/data1.js
@@ -63,8 +63,9 @@ query.once("value").then(function(snapshot) {
 
     var tr = document.createElement("tr");
     var value = childSnapshot.val();
+    var key = childSnapshot.key;
 
-    var trValues = [childSnapshot.key,value.name,value.startbid,value.currbid];
+    var trValues = [key,value.name,value.startbid,value.currbid];
 
     
 
@@ -76,30 +77,82 @@ query.once("value").then(function(snapshot) {
       // space.innerHTML = '<div> </div>';
       // var th = document.createElement("th");      // TABLE HEADER.
       // th.innerHTML = trValues[i];
-      td2.innerHTML =  '<input  style="width:80px; , padding-left:50px;" id="ex2" type="text">'+'&nbsp;' +'&nbsp;' +'&nbsp;' +'&nbsp;' +'&nbsp;' + '<button class="btn-primary"  id="placebidd" >' +  'Place Bid' + '</button>' ;
+      td2.innerHTML =  '<input placeholder="Bid amount" style="width:120px; , padding-left:50px;" id="ex2" type="text">'+'&nbsp;' +'&nbsp;' +'&nbsp;' +'&nbsp;' +'&nbsp;' + '<button class="btn-primary"  id="placebidd">' +  'Place Bid' + '</button>' ;
       td1.innerHTML =  trValues[i] ;
       tr.appendChild(td1);
       // console.log(this.find( 1).text());
+     
+     
       
       
     }
-
-    
-
+// onclick="alert(this.parentNode.parentNode.rowIndex)"
+// $('#placebidd').on('c')
+// alert( $(this).find('td:first').text());
     tr.appendChild(td2);
     // console.log(document.getElementById("proTable").rows.value);
-    
+    $(' table td:nth-child(2)').addClass('proname');
+
     table.appendChild(tr);
     table.className = 'table table-striped';
+  
 
+    $(".btn-primary").on('click' , (function() {
+      var $row = $(this).closest("tr");    // Find the row
+      var $text = $row.find("#ex2").val(); // Find the text
+      var $name = $row.find(".proname").text(); // Find the text
+
+      // Let's test it out
+      alert($text);
+      alert($name);
+  }))
     // placebidd.onclick = getId(this);
     
-    $('#proTable').on('click', 'tr', function() {
-       console.log( $(this).find('td:first').text());
+//     $('#proTable').on('click', 'tr', function() {
+//       //  console.log( $(this).find('td:first').text());
         
+// // console.log(key);
+
+//        var usersRef = firebase.database().ref('Products');
+
+//        var query = firebase.database().ref("Products").orderByChild('key').equalTo($(this).find('td:first').text());
+    
+//     query.on('value',function(snap) {
+//       console.log(key);
+//       console.log($(this).find('td:first').text());
+      
+//         snap.forEach(function(item) {
+//         console.log(key);
+        
+//            var current_item = item.val();
+
+//             console.log(current_item);
+            
+            
+//             // var hopperRef = usersRef.child(item.key);
+//             // hopperRef.update({
+//             //   "rtime": remainT
+//             // });
+
+//         });
+
+//      });
+//   });
   });
-  });
+
+  // $('#placebidd').on('click', function () {                
+  
+  //   var tbl = document.getElementById(proTable);
+  //   // alert($(this).closest('tr')[0].sectionRowIndex);
+  //   // console.log();
+    
+  //   alert($(this).closest('tr').find('#ex2').val());
+  
+  //   // var rCount = tbl.parentNode.parentNode.rows.length;
+   
+  // });
 });
+
 
 // var table = document.getElementById("proTable");
  
